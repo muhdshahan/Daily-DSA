9deac6c56066e59a3cd3f18e7194db81dcbc0750
muhdshahan <shahuuraff@gmail.com>
2025-12-30 11:19:58 +0530

    Problem - 1893

---
+++ 1893-cover-all-intergers-in-a-range/code.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        for i in range(left,right+1):
+            for r in ranges:
+                if i in range(r[0],r[1]+1):
+                    break
+                elif ranges[-1]==r:
+                    return False
+        return True
