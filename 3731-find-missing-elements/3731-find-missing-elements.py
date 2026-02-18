81401ee16d2b29699e642bb5cec3cceb7a344910
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-02-18 12:48:51 +0530

    Time: 3 ms (69.07%), Space: 19.3 MB (45.72%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -2,5 +2,4 @@ class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
         mini = min(nums)
         maxi = max(nums)
-        rslt = [i for i in range(mini,maxi) if i not in nums]
-        return rslt
+        return [i for i in range(mini,maxi) if i not in nums]
