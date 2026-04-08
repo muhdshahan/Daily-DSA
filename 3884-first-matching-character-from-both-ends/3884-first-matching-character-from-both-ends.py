83db10ac1ea81c83e2a0bb02a55c59225eea542e
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-04-08 19:31:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.py
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.py
@@ -1,6 +1,5 @@
 class Solution:
     def firstMatchingIndex(self, s: str) -> int:
-        print(s[0],s[len(s)-0-1])
         for i in range(len(s)):
             if s[i]==s[len(s)-i-1]:
                 return i
