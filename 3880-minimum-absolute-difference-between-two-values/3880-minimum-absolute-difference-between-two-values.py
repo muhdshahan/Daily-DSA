6a5d90e6c32f28898be9b112bb30809ded25700d
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-04-08 19:52:46 +0530

    Time: 13 ms (27.56%), Space: 19.3 MB (77.17%) - LeetHub

--- 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.py
+++ 3880-minimum-absolute-difference-between-two-values/3880-minimum-absolute-difference-between-two-values.py
@@ -10,3 +10,16 @@ class Solution:
                         if abs(i-j)<min_diff:
                             min_diff=abs(i-j)
         return min_diff
+        # new_dic = {}
+        # for i in range(len(nums)):
+        #     if nums[i] in [1,2]:
+        #         if nums[i] in new_dic:
+        #             new_dic[nums[i]].append(i)
+        #         else:
+        #             new_dic[nums[i]]=[i]
+        # print(new_dic)
+        # first = abs(min(new_dic[1])-min(new_dic[2]))
+        # second = abs(max(new_dic[1])-min(new_dic[2]))
+        # third = abs(max(new_dic[1])-max(new_dic[2]))
+        # fourth = abs(min(new_dic[1])-max(new_dic[2]))
+        # return min(first,second,third,fourth)
