b565b0e718447c0e439f05a2f59b808dcf26fe1f
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-04-16 21:36:03 +0530

    Time: 7 ms (35.57%), Space: 19.5 MB (20.64%) - LeetHub

--- 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.py
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.py
@@ -3,7 +3,6 @@ class Solution:
         nums = sorted(nums)
         tot_pairs = [sum(nums)]
         dicti = Counter(nums)
-        print(dicti)
         for j in dicti:
             pair = [nums[0]]
             if j>nums[0] and dicti[j]!=dicti[nums[0]]:
