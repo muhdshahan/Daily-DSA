5008961c6346f4f7228e8ca7f0bf396c392769b7
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-04-16 21:35:52 +0530

    Time: 12 ms (10.22%), Space: 19.5 MB (20.64%) - LeetHub

---
+++ 3852-smallest-pair-with-different-frequencies/3852-smallest-pair-with-different-frequencies.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minDistinctFreqPair(self, nums: list[int]) -> list[int]:
+        nums = sorted(nums)
+        tot_pairs = [sum(nums)]
+        dicti = Counter(nums)
+        print(dicti)
+        for j in dicti:
+            pair = [nums[0]]
+            if j>nums[0] and dicti[j]!=dicti[nums[0]]:
+                pair.append(j)
+                print(pair)
+                if sum(tot_pairs)>sum(pair):
+                    print("e",tot_pairs,pair)
+                    tot_pairs=pair
+                    print("e",tot_pairs,pair)
+            
+        return [-1,-1] if len(tot_pairs)<2 else tot_pairs
+
+            
