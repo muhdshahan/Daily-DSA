2a12e724ab9ab2d3926cba135a28e7268463fa38
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-04-29 12:27:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3827-count-monobit-integers/3827-count-monobit-integers.py
+++ 3827-count-monobit-integers/3827-count-monobit-integers.py
@@ -3,7 +3,6 @@ class Solution:
         count = 0
         for i in range(n+1):
             binary = str(bin(i)[2:])
-            print(binary)
             if "0" in binary and "1" in binary:
                 continue
             else:
