8f3096f4a2e70bcb953a9e9c8050b51d4ff12fc6
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-05-12 16:21:31 +0530

    Time: 11 ms (7.94%), Space: 19.3 MB (51.5%) - LeetHub

--- 3803-count-residue-prefixes/3803-count-residue-prefixes.py
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.py
@@ -2,8 +2,6 @@ class Solution:
     def residuePrefixes(self, s: str) -> int:
         rslt = 0
         for win_len in range(len(s)):
-            prefix = s[:win_len+1]
-            distinct_char = set(prefix)
-            if len(distinct_char)==len(prefix)%3:
+            if len(set(s[:win_len+1]))==len(s[:win_len+1])%3:
                 rslt += 1
         return rslt
