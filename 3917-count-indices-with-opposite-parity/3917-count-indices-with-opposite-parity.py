a316585279b95145fd0649b7d3f47cf0e7d291ee
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-05-21 19:02:05 +0530

    Time: 33 ms (30.22%), Space: 19.3 MB (30.55%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.py
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.py
@@ -12,7 +12,6 @@ class Solution:
             for j in range(i+1,len(parity)):
                 if parity[i]!=parity[j]:
                     count+=1
-                print(parity[i],parity[j],count)
             rslt.append(count)
         rslt.append(0)
         return rslt
