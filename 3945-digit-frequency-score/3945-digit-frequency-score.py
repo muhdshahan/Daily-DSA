3ca0f6923f8d21838ea595ade9f7902e74f0964b
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-06-16 10:15:24 +0530

    Time: 0 ms (100%), Space: 19.2 MB (67.49%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.py
+++ 3945-digit-frequency-score/3945-digit-frequency-score.py
@@ -2,8 +2,7 @@ class Solution:
     def digitFrequencyScore(self, n: int) -> int:
         n = str(n)
         freq_pairs = Counter(n)
-        n = set(n)
         rslt = 0
-        for i in n:
+        for i in freq_pairs:
             rslt += int(i) * freq_pairs[i]
         return rslt
