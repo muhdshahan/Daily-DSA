abe83b32b65313be9c63ae3489a895d11f1f050f
Muhammed Shahan P P <shahuuraff@gmail.com>
2026-06-17 09:52:40 +0530

    Time: 0 ms (100%), Space: 19.2 MB (56.6%) - LeetHub

---
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumIndex(self, capacity: list[int], itemSize: int) -> int:
+        min_value = 999
+        min_index = 999
+        for i in range(len(capacity)):
+            if capacity[i]>=itemSize:
+                if min_value > capacity[i]:
+                    print(min_value, min_index)
+                    min_value = capacity[i]
+                    min_index = i
+        return min_index if min_index != 999 else -1
